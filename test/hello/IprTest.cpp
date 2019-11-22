28bca72883461d8f65e488888fcfa9dddf425a24
Gor Nishanov <GorNishanov@gmail.com>
2019-11-22 13:50:16 -0800

    Added clang-format and reformat test files

--- test/hello/IprTest.cpp
+++ test/hello/IprTest.cpp
@@ -27,175 +27,176 @@ int main() {
 }
 #endif
 
-struct value_dep_builder
-{
-  impl::Lexicon fac;
-  impl::Translation_unit tu {fac};
-  Printer pp{std::cout};
+struct value_dep_builder {
+   impl::Lexicon fac;
+   impl::Translation_unit tu{fac};
+   Printer pp{std::cout};
 
-  ipr::Decl* X {};
-  ipr::Decl* a_fn{};
-  ipr::Decl* a_tmpl{};
+   ipr::Decl* X{};
+   ipr::Decl* a_fn{};
+   ipr::Decl* a_tmpl{};
 
-//    template<auto N> struct X {
-//        operator int() const { return 0; }
-//    };
-//
-  impl::Class* build_X()
-  {
+   //    template<auto N> struct X {
+   //        operator int() const { return 0; }
+   //    };
+   //
+   impl::Class* build_X() {
       // wny not global_region->make_mapping?
-    auto *mapping = fac.make_mapping(*tu.global_region());
-    auto *N = fac.make_parameter(fac.get_identifier("N"), fac.get_auto(), *mapping);
+      auto* mapping = fac.make_mapping(*tu.global_region());
+      auto* N = fac.make_parameter(fac.get_identifier("N"), fac.get_auto(), *mapping);
       // should be done at commit. Since once we got the type of paramters,
       // we should not be adding more.
-    auto& templ = fac.get_template(mapping->parameters.type(), fac.class_type());
-    mapping->constraint = &templ;
+      auto& templ = fac.get_template(mapping->parameters.type(), fac.class_type());
+      mapping->constraint = &templ;
 
-    auto *X = tu.global_region()->declare_primary_map(fac.get_identifier("X"), templ);
-    X->init = mapping;
-    auto *args = fac.make_expr_list();
-    args->push_back(N);
-    this->X = X;
+      auto* X = tu.global_region()->declare_primary_map(fac.get_identifier("X"), templ);
+      X->init = mapping;
+      auto* args = fac.make_expr_list();
+      args->push_back(N);
+      this->X = X;
 
-    auto *body = fac.make_class(mapping->params());
-    mapping->body = body;
+      auto* body = fac.make_class(mapping->params());
+      mapping->body = body;
 
-    auto& n = *fac.make_id_expr(*X);
-    auto& a = *args;
+      auto& n = *fac.make_id_expr(*X);
+      auto& a = *args;
 
-    body->id = &fac.get_template_id(n, a);
-    X_add_conversion_to_int(body);
+      body->id = &fac.get_template_id(n, a);
+      X_add_conversion_to_int(body);
 
-    return body;
-  }
-  void X_add_conversion_to_int(impl::Class* owner)
-  {
-//        operator int() const { return 0; }
-//    operator int: (this:
-    auto& int_type = fac.int_type();
-    auto* mapping = fac.make_mapping(owner->region());
-    auto& const_class = fac.get_qualified(Type_qualifier::Const, *owner);
-    auto& const_ptr = fac.get_pointer(const_class);
-    auto* this_param = fac.make_parameter(fac.get_identifier("this"), const_ptr, *mapping);
-    auto& fn_type = fac.get_function(mapping->parameters.type(), int_type);
-    mapping->constraint = &fn_type;
+      return body;
+   }
+   void X_add_conversion_to_int(impl::Class* owner) {
+      //        operator int() const { return 0; }
+      //    operator int: (this:
+      auto& int_type = fac.int_type();
+      auto* mapping = fac.make_mapping(owner->region());
+      auto& const_class = fac.get_qualified(Type_qualifier::Const, *owner);
+      auto& const_ptr = fac.get_pointer(const_class);
+      auto* this_param
+          = fac.make_parameter(fac.get_identifier("this"), const_ptr, *mapping);
+      auto& fn_type = fac.get_function(mapping->parameters.type(), int_type);
+      mapping->constraint = &fn_type;
 
-    auto* int_conv = owner->declare_fun(fac.get_conversion(fac.int_type()), fn_type);
-    int_conv->init = mapping;
+      auto* int_conv = owner->declare_fun(fac.get_conversion(fac.int_type()), fn_type);
+      int_conv->init = mapping;
 
-    auto* body = fac.make_block(mapping->parameters, int_type);
-    mapping->body = body;
+      auto* body = fac.make_block(mapping->parameters, int_type);
+      mapping->body = body;
 
-    auto& zero = fac.get_literal(int_type, "0");
-    body->add_stmt(fac.make_return(zero));
-}
+      auto& zero = fac.get_literal(int_type, "0");
+      body->add_stmt(fac.make_return(zero));
+   }
 
-//  int a(int) { return 1; }
+   //  int a(int) { return 1; }
 
-  void build_a_int() {
-    auto& int_type = fac.int_type();
-    auto* owner = tu.global_region();
+   void build_a_int() {
+      auto& int_type = fac.int_type();
+      auto* owner = tu.global_region();
 
-    auto* mapping = fac.make_mapping(*owner);
-    fac.make_parameter(fac.get_identifier(""), int_type, *mapping);
-    auto& fn_type = fac.get_function(mapping->parameters.type(), int_type);
-    mapping->constraint = &fn_type;
+      auto* mapping = fac.make_mapping(*owner);
+      fac.make_parameter(fac.get_identifier(""), int_type, *mapping);
+      auto& fn_type = fac.get_function(mapping->parameters.type(), int_type);
+      mapping->constraint = &fn_type;
 
-    auto* a_fn = owner->declare_fun(fac.get_identifier("a"), fn_type);
-    a_fn->init = mapping;
+      auto* a_fn = owner->declare_fun(fac.get_identifier("a"), fn_type);
+      a_fn->init = mapping;
 
-    auto* body = fac.make_block(mapping->parameters, int_type);
-    mapping->body = body;
+      auto* body = fac.make_block(mapping->parameters, int_type);
+      mapping->body = body;
 
-    auto& one = fac.get_literal(int_type, "1");
-    body->add_stmt(fac.make_return(one));
+      auto& one = fac.get_literal(int_type, "1");
+      body->add_stmt(fac.make_return(one));
 
-    this->a_fn = a_fn;
-  }
+      this->a_fn = a_fn;
+   }
 
-  // template<typename T>
-  // auto g(T t) {
-  //     constexpr auto n = sizeof sizeof t - sizeof(decltype(0));
-  //     X<n> x;
-  //     return a(x);
-  // }
+   // template<typename T>
+   // auto g(T t) {
+   //     constexpr auto n = sizeof sizeof t - sizeof(decltype(0));
+   //     X<n> x;
+   //     return a(x);
+   // }
 
-  // ipr=impl: FIXME: make_parameter should be on mapping.
+   // ipr=impl: FIXME: make_parameter should be on mapping.
 
-  void build_g_T() {
-    auto* owner = tu.global_region();
+   void build_g_T() {
+      auto* owner = tu.global_region();
 
-    auto* mapping = fac.make_mapping(*owner);
-    auto* T = fac.make_parameter(fac.get_identifier("T"), fac.typename_type(), *mapping);
-    auto& template_ty = fac.get_template(mapping->parameters.type(), fac.typename_type());
-    mapping->constraint = &template_ty;
+      auto* mapping = fac.make_mapping(*owner);
+      auto* T
+          = fac.make_parameter(fac.get_identifier("T"), fac.typename_type(), *mapping);
+      auto& template_ty
+          = fac.get_template(mapping->parameters.type(), fac.typename_type());
+      mapping->constraint = &template_ty;
 
-    auto* mapping2 = fac.impl::expr_factory::make_mapping(mapping->parameters, fac.typename_type(), 1);
-    auto* t = fac.make_parameter(fac.get_identifier("t"), fac.get_as_type(*T), *mapping2);
-    auto& fn_ty = fac.get_function(mapping2->parameters.type(), fac.get_auto());
-    mapping2->constraint = &fn_ty;
+      auto* mapping2 = fac.impl::expr_factory::make_mapping(
+          mapping->parameters, fac.typename_type(), 1);
+      auto* t
+          = fac.make_parameter(fac.get_identifier("t"), fac.get_as_type(*T), *mapping2);
+      auto& fn_ty = fac.get_function(mapping2->parameters.type(), fac.get_auto());
+      mapping2->constraint = &fn_ty;
 
-    auto *X = tu.global_region()->declare_primary_map(fac.get_identifier("g"), template_ty);
-    X->init = mapping;
-    //X->args.push_back(T);
+      auto* X
+          = tu.global_region()->declare_primary_map(fac.get_identifier("g"), template_ty);
+      X->init = mapping;
+      //X->args.push_back(T);
 
-    mapping->body = mapping2;
+      mapping->body = mapping2;
 
-    auto* body = fac.make_block(mapping2->parameters, fn_ty.target());
-    mapping2->body = body;
+      auto* body = fac.make_block(mapping2->parameters, fn_ty.target());
+      mapping2->body = body;
 
-    auto* n = build_n_decl(*body, *t);
-    body->add_stmt(n);
-    auto* x = build_x_decl(*body, *n);
-    body->add_stmt(x);
-    // build f(x) TODO: conv operator
-    auto* args = fac.make_expr_list();
-    args->push_back(fac.make_id_expr(*x));
-    auto* call = fac.make_call(*fac.make_id_expr(*a_fn), *args);
-    body->add_stmt(fac.make_return(*call));
-  }
+      auto* n = build_n_decl(*body, *t);
+      body->add_stmt(n);
+      auto* x = build_x_decl(*body, *n);
+      body->add_stmt(x);
+      // build f(x) TODO: conv operator
+      auto* args = fac.make_expr_list();
+      args->push_back(fac.make_id_expr(*x));
+      auto* call = fac.make_call(*fac.make_id_expr(*a_fn), *args);
+      body->add_stmt(fac.make_return(*call));
+   }
 
-  //     constexpr auto n = sizeof sizeof t - sizeof(decltype(0));
-  ipr::Decl const* build_n_decl(impl::Block& body, ipr::Decl const& t) {
-    auto* x = fac.make_sizeof(*fac.make_sizeof(*fac.make_id_expr(t))); // FIXME: compute sizes and types
-    auto& dclt = fac.get_decltype(fac.get_literal(fac.int_type(), "0"));
-    auto* y = fac.make_sizeof(dclt);
-    auto* init = fac.make_minus(*x,*y);
+   //     constexpr auto n = sizeof sizeof t - sizeof(decltype(0));
+   ipr::Decl const* build_n_decl(impl::Block& body, ipr::Decl const& t) {
+      auto* x = fac.make_sizeof(
+          *fac.make_sizeof(*fac.make_id_expr(t))); // FIXME: compute sizes and types
+      auto& dclt = fac.get_decltype(fac.get_literal(fac.int_type(), "0"));
+      auto* y = fac.make_sizeof(dclt);
+      auto* init = fac.make_minus(*x, *y);
 
-    auto* n = body.region.declare_var(fac.get_identifier("n"), fac.get_auto());
-    n->init = init;
+      auto* n = body.region.declare_var(fac.get_identifier("n"), fac.get_auto());
+      n->init = init;
 
-    return n;
-  }
+      return n;
+   }
 
-  //     X<n> x; // x: X<n> -- need to request specialization.
-  ipr::Decl const* build_x_decl(impl::Block& body, ipr::Decl const& n){
-    // build X<n>
-    //impl::Expr_list args; // BUG. Why it is allowed?
-    auto *args = fac.make_expr_list();
-    args->push_back(fac.make_id_expr(n));
-    auto& tmpl_id = fac.get_template_id(*fac.make_id_expr(*X), *args);
-    // FIXME: Need to do specialization.
-    auto& specialization = fac.get_as_type(tmpl_id);
-    auto *x = body.region.declare_var(fac.get_identifier("x"), specialization);
-    // TODO: a constructor call.
-    return x;
-  }
+   //     X<n> x; // x: X<n> -- need to request specialization.
+   ipr::Decl const* build_x_decl(impl::Block& body, ipr::Decl const& n) {
+      // build X<n>
+      //impl::Expr_list args; // BUG. Why it is allowed?
+      auto* args = fac.make_expr_list();
+      args->push_back(fac.make_id_expr(n));
+      auto& tmpl_id = fac.get_template_id(*fac.make_id_expr(*X), *args);
+      // FIXME: Need to do specialization.
+      auto& specialization = fac.get_as_type(tmpl_id);
+      auto* x = body.region.declare_var(fac.get_identifier("x"), specialization);
+      // TODO: a constructor call.
+      return x;
+   }
 
-
-  value_dep_builder()
-  {
-    build_X();
-    build_a_int();
-    build_g_T();
-    pp << tu;
-  }
+   value_dep_builder() {
+      build_X();
+      build_a_int();
+      build_g_T();
+      pp << tu;
+   }
 };
 
-int main()
-{
-  value_dep_builder b;
-  b.pp << "Starting work on value dep example 3\n";
+int main() {
+   value_dep_builder b;
+   b.pp << "Starting work on value dep example 3\n";
 }
 
 #if 0
--- test/templ/TemplTest.cpp
+++ test/templ/TemplTest.cpp
@@ -26,25 +26,67 @@
 using namespace ipr;
 
 namespace {
-	struct builder
-	{
-		Printer pp{std::cout};
+   struct builder {
+      Printer pp{std::cout};
 
-		impl::Lexicon fac;
-		impl::Translation_unit tu {fac};
-		impl::Region *active_region = tu.global_region();
+      impl::Lexicon fac;
+      impl::Translation_unit tu{fac};
+      impl::Region* active_region = tu.global_region();
+      File_index file_index = fac.get_fileindex(fac.get_string("<source>"));
 
-		void build_forward_declaration_of_S();
-	};
-}
+      void build_forward_declaration_of_S();
+      void build_definition_of_S();
+
+      builder() {
+         build_forward_declaration_of_S();
+         build_definition_of_S();
+      }
+   };
+} // namespace
 //  1 template <typename T>
-//  2 struct S;
-
+//  2 struct S; -- ; is at pos <2:9>
 void builder::build_forward_declaration_of_S() {
+   auto* mapping = fac.make_mapping(*active_region);
+   auto* T = fac.make_parameter(fac.get_identifier("T"), fac.typename_type(), *mapping);
+   auto& templ = fac.get_template(mapping->parameters.type(), fac.class_type());
+   mapping->constraint = &templ;
+   pp << "template-type: " << templ << " " << templ.node_id << "\n";
 
+   auto* S = active_region->declare_primary_map(fac.get_identifier("S"), templ);
+   S->init = mapping;
+   S->args.push_back(T);
+   S->src_locus = ipr::Source_location{{Line_number{1}, Column_number{1}}, file_index};
+   pp << *S << "\n";
+}
+
+//  4 template <typename T>
+//  5 struct S {
+//  6   template <typename U>
+//  7   struct Q;
+//  8 };
+void builder::build_definition_of_S() {
+   auto* mapping = fac.make_mapping(*active_region);
+   auto* T = fac.make_parameter(fac.get_identifier("T"), fac.typename_type(), *mapping);
+   auto& templ = fac.get_template(mapping->parameters.type(), fac.class_type());
+   mapping->constraint = &templ;
+   pp << "template-type: " << templ << " " << templ.node_id << "\n";
+
+   auto* S = active_region->declare_primary_map(fac.get_identifier("S"), templ);
+   S->init = mapping;
+   S->args.push_back(T);
+   S->src_locus = ipr::Source_location{{Line_number{4}, Column_number{1}}, file_index};
+
+   auto* body = fac.make_class(mapping->params());
+   mapping->body = body;
+
+   auto& n = *fac.make_id_expr(*S);
+   auto& a = S->args;
+
+   body->id = &fac.get_template_id(n, a);
+   pp << *S << "\n";
 }
 
 int main() {
-	std::cout << "Hi\n";
-	builder b;
+   std::cout << "Hi\n";
+   builder b;
 }
